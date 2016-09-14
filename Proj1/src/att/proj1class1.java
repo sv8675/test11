5aed9ccee2e34ef37dfac8071a4ef1815a1b8047
sv8675 <sv8675@att.com>
2016-09-14 13:35:57 -0500

    Initial 2 project checkin

---
+++ Proj1/src/att/proj1class1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package att;
+
+/**
+ * @author sv8675
+ *
+ */
+public class proj1class1 {
+
+	/**
+	 * 
+	 */
+	public proj1class1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello1");
+	}
+
+}
---
+++ Proj2/src/att/proj2class1.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package att;
+
+/**
+ * @author sv8675
+ *
+ */
+public class proj2class1 {
+
+	/**
+	 * 
+	 */
+	public proj2class1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello2");
+
+	}
+
+}
